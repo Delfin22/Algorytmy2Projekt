1efcd2a381332c00fd07057755dd07d465519257
Cezary Arszyński <czarek.arszynski@gmail.com>
2024-04-21 18:42:17 +0200

    1.0.2 World generator + JFrame

--- src/main/java/swing/components/FrameComponent.java
+++ src/main/java/swing/components/FrameComponent.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 import java.util.List;
 
 public class FrameComponent extends JFrame {
-    private static FrameComponent frame = new FrameComponent();
+    private final static FrameComponent frame = new FrameComponent();
     public static void printFrame(List<Point> points){
         frame.setSize(1920,1080);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
