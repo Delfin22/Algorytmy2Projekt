38ab9bec751bf2b841ec85b7a7e55eaf53f7d328
Delfin22 <czarek.arszynski@gmail.com>
2024-04-24 13:08:01 +0200

    Update JUnit and refactor main package.

--- src/main/java/org/algorithmProject/Main.java
+++ src/main/java/org/algorithmProject/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.algorithmProject;
 
 import classes.Point;
 import classes.PointGenerator;
--- src/test/java/ToolsTest.java
+++ src/test/java/ToolsTest.java
@@ -1,8 +1,7 @@
 import classes.Point;
 import classes.Tools;
-import org.junit.Assert;
-import org.junit.Test;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 
 public class ToolsTest {
@@ -11,11 +10,11 @@ public class ToolsTest {
         Point p1 = new Point(1,2);
         Point p2 = new Point(3,4);
         Point p3 = new Point(5,6);
-        Assert.assertEquals(0, Tools.calculateDet(p1,p2,p3),0);
+        Assertions.assertEquals(0, Tools.calculateDet(p1,p2,p3),0);
 
         p1 = new Point(3,5);
         p2 = new Point(4,1);
         p3 = new Point(12,6);
-        Assert.assertEquals(37, Tools.calculateDet(p1,p2,p3),0);
+        Assertions.assertEquals(37, Tools.calculateDet(p1,p2,p3),0);
     }
 }
