bdcb562316aa4dddf96b44c3f182c1e6e6cd1bd0
Cezary Arszyński <czarek.arszynski@gmail.com>
2024-05-01 13:21:47 +0200

    Implementation of Karp-Rabin algorithm + few tests of critical scenarios

--- src/main/java/classes/PatternTools.java
+++ src/main/java/classes/PatternTools.java
@@ -1,12 +1,58 @@
 package classes;
 
-public class PatternTools {
+import java.util.ArrayList;
+import java.util.List;
 
-    public static boolean searchPattern(String s1, String pattern) throws IllegalArgumentException{
-        if(s1.isEmpty())
+public class PatternTools {
+    private final static int lettersInAlphabet = 26; //using small letters in latin alphabet
+    private final static int modNumber = 29; //"good prime number"
+
+    public static List<Integer> searchPattern(String text, String pattern) throws IllegalArgumentException{
+
+        /* Karp - Rabin algorithm */
+
+        List <Integer> indexes = new ArrayList<>();
+        int textLength = text.length();
+        int patternLength = pattern.length();
+        int h = (int) Math.pow(lettersInAlphabet,patternLength - 1) % modNumber;
+
+        int p = 0, t0 = 0;
+        int j,s;
+
+        //possible exceptions
+        if(textLength == 0 || patternLength == 0 || patternLength > textLength){
             throw new IllegalArgumentException();
-        if(s1.contains(pattern))
-            return true;
-        return false;
+        }
+
+        //hashing
+        for(int i = 0; i < patternLength; i++){
+            p = (lettersInAlphabet*p + pattern.charAt(i))%modNumber;
+            t0 = (lettersInAlphabet*t0 + text.charAt(i))%modNumber;
+        }
+
+        for(s = 0; s <= (textLength-patternLength); s++){
+            if(p == t0){
+                //by characters
+                for(j = 0; j < patternLength; j++){
+                    if(pattern.charAt(j) != text.charAt(s+j)) { //if chars are not equal
+                        break;
+                    }
+                }
+                if(j == patternLength) { //if previous loop was executed without break
+                    indexes.add(s);
+                }
+            }
+
+            //hashing
+            if(s < (textLength - patternLength)){
+                t0 = (lettersInAlphabet*(t0 - text.charAt(s) * h) + text.charAt(s+patternLength))%modNumber;
+
+                //convert negative values//
+                if (t0 < 0) {
+                    t0 = (t0 + modNumber);
+                }
+            }
+        }
+        return indexes;
     }
 }
--- src/test/java/PatternToolsTest.java
+++ src/test/java/PatternToolsTest.java
@@ -2,22 +2,38 @@ import classes.PatternTools;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PatternToolsTest {
-    String pattern = "boli";
 
     @Test
     public void patternSearchTest(){
-       String s1 = "kjjn bolimodsarfizm asmlkd boli assa boli sad";
-       String s2 = new String();
-       Assertions.assertEquals(true, PatternTools.searchPattern(s1,pattern));
+        String pattern = "boli";
+        String s1 = "kjjn bolimodsarfizm asmlkd boli assa boli sad";
+        String emptyString = "";
 
-       Assertions.assertEquals(true, PatternTools.searchPattern(s1,s1));
+        List<Integer> list = new ArrayList<>(3);
+        list.add(5);
+        list.add(27);
+        list.add(37);
+        //Searching "boli". Expected at indexes above.
+        Assertions.assertEquals(list, PatternTools.searchPattern(s1, pattern));
 
-       Assertions.assertEquals(true, PatternTools.searchPattern(s1,s2));
+        List<Integer> list1 = new ArrayList<>(1);
+        list1.add(0);
+        //Searching text in text - expected at index 0
+        Assertions.assertEquals(list1, PatternTools.searchPattern(s1, s1));
 
-       Assertions.assertThrows(IllegalArgumentException.class, () -> PatternTools.searchPattern(s2,pattern));
-       Assertions.assertThrows(IllegalArgumentException.class, () -> PatternTools.searchPattern(s2,s2));
+        //empty pattern
+        Assertions.assertThrows(IllegalArgumentException.class, () -> PatternTools.searchPattern(s1, emptyString));
+        //empty text
+        Assertions.assertThrows(IllegalArgumentException.class, () -> PatternTools.searchPattern(emptyString, s1));
+        //empty pattern and text
+        Assertions.assertThrows(IllegalArgumentException.class, () -> PatternTools.searchPattern(emptyString, emptyString));
+
+        //pattern longer than text
+        Assertions.assertThrows(IllegalArgumentException.class, () -> PatternTools.searchPattern("text", "Longer Pattern"));
+
     }
-
-
 }
