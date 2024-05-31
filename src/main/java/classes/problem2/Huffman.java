8cf9154e278e1d9d97f43ddeff70bcefa1615db4
kenny <tomoj@email.com>
2024-05-31 23:52:15 +0200

    Finished Huffman hashmap coding

--- src/main/java/classes/problem2/Huffman.java
+++ src/main/java/classes/problem2/Huffman.java
@@ -3,21 +3,21 @@ package classes.problem2;
 import java.util.*;
 
 public class Huffman {
-    public static Node createTree(String text){
+    public static Node createTree(String text) {
         int n = PatternTools.getLettersInAlphabet();
         int index;
         PriorityQueue<Node> q = new PriorityQueue<>(n, Comparator.comparingInt(Node::getFreq));
         List<Integer> freqList = new ArrayList<>(Collections.nCopies(n, 0)); //initialise with 0s
 
         //count letters
-        for(int i = 0; i < text.length(); i++){
+        for (int i = 0; i < text.length(); i++) {
             index = text.charAt(i) - PatternTools.getFirstLetter();
-            freqList.set(index,freqList.get(index)+1);
+            freqList.set(index, freqList.get(index) + 1);
         }
 
         //create nodes
-        for(int i = 0; i < n; i++){
-            if(freqList.get(i) != 0) {
+        for (int i = 0; i < n; i++) {
+            if (freqList.get(i) != 0) {
                 Node node = new Node();
                 node.setLetter((char) (PatternTools.getFirstLetter() + i));
                 node.setFreq(freqList.get(i));
@@ -26,14 +26,14 @@ public class Huffman {
         }
 
         Node root = null;
-        while(q.size() > 1){
+        while (q.size() > 1) {
             Node x = q.peek();
             q.poll();
             Node y = q.peek();
             q.poll();
 
             Node f = new Node();
-            f.setFreq(x.getFreq()+y.getFreq());
+            f.setFreq(x.getFreq() + y.getFreq());
             f.setLetter('-');
             f.setLeftChild(x);
             f.setRightChild(y);
@@ -43,26 +43,36 @@ public class Huffman {
         return root;
     }
 
-    public static LinkedHashMap<Character,String> createHashMap(Node root){
+    public static LinkedHashMap<Character, String> createHashMap(Node root) {
+        LinkedHashMap<Character, String> codes = new LinkedHashMap<>();
+
         Stack<Node> stack = new Stack<>();
         Node node = root;
-
-        while(node != null || !stack.isEmpty()){
-            while(node != null) {
+        Stack<StringBuilder> codeStack = new Stack<>();
+        StringBuilder currCode = new StringBuilder();
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
                 stack.add(node);
+                codeStack.add(new StringBuilder(currCode));  // ensure independent path code
+                currCode.append('0');
                 node = node.getLeftChild();
             }
             node = stack.pop();
+            currCode = codeStack.pop();
 
-            if(node.getLeftChild() == null && node.getRightChild() == null && Character.isLetter(node.getLetter())){
-               System.out.println(node.getLetter() + " " + node.getFreq() + " ");
+            if (node.getLeftChild() == null && node.getRightChild() == null && Character.isLetter(node.getLetter())) {
+                // System.out.println("- " + node.getLetter() + " " + node.getFreq() + " " + currCode);
+                codes.put(node.getLetter(), currCode.toString());
             }
+
             node = node.getRightChild();
+            currCode.append('1');
         }
 
-        return null;
+        return codes;
     }
-    public static String encodeText(String text){
+
+    public static String encodeText(String text) {
         Node root = createTree(text);
         return null;
     }
--- src/main/java/org/algorithmProject/Main.java
+++ src/main/java/org/algorithmProject/Main.java
@@ -1,13 +1,18 @@
 package org.algorithmProject;
+
 import classes.problem2.Huffman;
 import classes.problem2.Node;
+
+import java.lang.reflect.MalformedParameterizedTypeException;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         Node node = Huffman.createTree("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbccccccccccccddddddddddddddddfffffeeeeeeeee");
         node.print("");
         System.out.println();
-        Huffman.createHashMap(node);
 
+        Map<Character, String> letter_codes = Huffman.createHashMap(node);
+        letter_codes.forEach(((character, code) -> System.out.printf("%c %s\n", character, code)));
     }
 }
